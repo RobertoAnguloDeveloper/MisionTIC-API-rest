352d51c7c8a68a2f41bed78ac61083753b1fed0a
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-09-25 22:31:36 -0500

    Reto 2 - Llamados ajax

--- ajax.js
+++ ajax.js
@@ -1,12 +1,34 @@
 function getClients() {
-    //Get request
     $.ajax({
-        url: 'https://g7913fe229aa44b-jljr5f68ioqy3a4a.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
+        url: 'https://g7913fe229aa44b-proyecto23.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         type: 'GET',
         dataType: 'json',
         
         success: function (list) {
             console.log(list.items);
+
+            if (document.getElementById('table')) {
+                document.getElementById('table').remove();
+            }
+
+            table = document.createElement("table");
+            document.body.appendChild(table);
+            table.id = "table";
+            document.getElementById("table").setAttribute("class", "table table-striped table-dark w-50 p-3");
+            thead = document.createElement("thead");
+            tr = document.createElement("tr");
+            thead.appendChild(tr);
+            tr.innerHTML = "<th>ID</th><th>NAME</th><th>EMAIL</th><th>AGE</th><th>ACTIONS</th>";
+            table.appendChild(thead);
+
+            table = document.getElementById('table');
+
+            for (let i = 0; i < list.items.length; i++) {
+                table.insertRow(-1).innerHTML = '<td>' + list.items[i].id + '</td>' 
+                                                + '<td>' + list.items[i].name + '</td>'
+                                                + '<td>' + list.items[i].email + '</td>'
+                                                + '<td>' + list.items[i].age + '</td>';
+            }
         },
         error: function (xhr, status) {
             console.log(status);
@@ -32,20 +54,21 @@ function createClient() {
     let clientJson = JSON.stringify(client);
 
     $.ajax({
-        url: 'https://g7913fe229aa44b-jljr5f68ioqy3a4a.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
+        url: 'https://g7913fe229aa44b-proyecto23.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         type: 'POST',
         dataType: 'json',
         data: clientJson,
         contentType: 'application/json',
 
-        success : function () {
+        error : function (xhr, status) {
+            //console.log("PILLA");
+        },
+        complete : function () {
             $("#client_id").val("");
             $("#client_name").val("");
             $("#client_email").val("");
             $("#client_age").val("");
-        },
-        error : function (xhr, status) {
-            console.log(xhr);
+            getClients();
         }
     });
 }
