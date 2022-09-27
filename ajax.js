67876bfca10cf4dc68ae7da223e4da384bada63a
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-09-26 20:20:52 -0500

    Editable table

--- ajax.js
+++ ajax.js
@@ -1,3 +1,5 @@
+var clients;
+
 function getClients() {
     $.ajax({
         url: 'https://g7913fe229aa44b-proyecto23.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
@@ -6,32 +8,26 @@ function getClients() {
         
         success: function (list) {
             console.log(list.items);
+            clients = list.items;
 
-            if (document.getElementById('table')) {
-                document.getElementById('table').remove();
+            if (document.getElementById("client_table").querySelector("tbody")) {
+                document.getElementById("client_table").querySelector("tbody").remove();
             }
 
-            table = document.createElement("table");
-            document.body.appendChild(table);
-            table.id = "table";
-            document.getElementById("table").setAttribute("class", "table table-striped table-dark w-50 p-3");
-            thead = document.createElement("thead");
-            tr = document.createElement("tr");
-            thead.appendChild(tr);
-            tr.innerHTML = "<th>ID</th><th>NAME</th><th>EMAIL</th><th>AGE</th><th>ACTIONS</th>";
-            table.appendChild(thead);
-
-            table = document.getElementById('table');
-
             for (let i = 0; i < list.items.length; i++) {
-                table.insertRow(-1).innerHTML = '<td>' + list.items[i].id + '</td>' 
-                                                + '<td>' + list.items[i].name + '</td>'
-                                                + '<td>' + list.items[i].email + '</td>'
-                                                + '<td>' + list.items[i].age + '</td>';
+                if (i == 0) {
+                    tbody = document.createElement("tbody");
+                    document.getElementById("client_table").appendChild(tbody);
+                }
+                document.getElementById("client_table").querySelector("tbody").insertRow(-1).innerHTML = '<td>' + list.items[i].id + '</td>' 
+                                                + '<td><div contenteditable>' + list.items[i].name + '</div></td>'
+                                                + '<td><div contenteditable>' + list.items[i].email + '</div></td>'
+                                                + '<td><div contenteditable>' + list.items[i].age + '</div></td>'
+                                                + '<td><BUTTON class = "btn-primary rounded">SAVE</BUTTON><BUTTON class = "btn-danger ml-2 rounded">DELETE</BUTTON></td>';
             }
         },
         error: function (xhr, status) {
-            console.log(status);
+            //console.log(status);
         }
 
     });
